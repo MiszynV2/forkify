3bf2c3c8bbf3b1e7b66f026fe416cbd417f1221d
Miszyn <wmarciniszyn11@gmail.com>
2021-07-22 17:30:22 +0200

    Add functionality to upload a recipe

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,10 +1,12 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import { TIMEOUT_MILISEC } from './config.js';
 //import View from './View.js'
 import resultsView from './views/resultsView.js';
 import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
+import addRecipeView from './views/addRecipeView.js';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -20,8 +22,8 @@ const controlRecipes = async function () {
 
     resultsView.update(model.getSearchResultPage());
 
-  //0) render bookmarks
-  bookmarksView.render(model.state.bookmarks)
+    //0) render bookmarks
+    bookmarksView.render(model.state.bookmarks);
 
     //1
 
@@ -29,10 +31,10 @@ const controlRecipes = async function () {
     //(2)Rendering recipe
     recipeView.render(model.state.recipe);
     //3) updating bookmarks view
-    bookmarksView.update(model.state.bookmarks)
+    bookmarksView.update(model.state.bookmarks);
   } catch (err) {
     recipeView.renderError();
-    console.error(err)
+    console.error(err);
   }
 };
 
@@ -72,19 +74,36 @@ const controlAddBookmark = function () {
   if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
   else model.deleteBookmark(model.state.recipe.id);
   //2) update recipe view
-  recipeView.update(model.state.recipe)
-
+  recipeView.update(model.state.recipe);
 };
-const controlBookmarks = function(){
-  bookmarksView.render(model.state.bookmarks)
-}
+const controlBookmarks = function () {
+  bookmarksView.render(model.state.bookmarks);
+};
+const controlAddRecipe = async function (newRecipe) {
+  try {
+    addRecipeView.renderSpinner();
+    //upload new recipe data
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+    recipeView.render(model.state.recipe);
+    addRecipeView.renderMessage();
+
+    setTimeout(function () {
+      addRecipeView.toggleWindow();
+    }, TIMEOUT_MILISEC);
+  } catch (err) {
+    console.error('BOOM', err);
+    addRecipeView.renderError(err);
+  }
+};
 
 const init = function () {
-  bookmarksView.addHandlerRender(controlBookmarks)
+  bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
   recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
+  addRecipeView._addHandlerUpload(controlAddRecipe);
 };
 init();
--- src/js/model.js
+++ src/js/model.js
@@ -1,6 +1,6 @@
 import { async } from 'regenerator-runtime';
-import { API_URL, RES_PER_PAGE } from './config.js';
-import { getJSON } from './helpers.js';
+import { API_URL, RES_PER_PAGE, KEY} from './config.js';
+import { getJSON, sendJSON } from './helpers.js';
 
 export const state = {
   recipe: {},
@@ -13,22 +13,28 @@ export const state = {
   bookmarks: [],
 };
 
+const createRecipeObject = function(data){
+  const recipe = data.data.recipe;
+  return {
+    id: recipe.id,
+    title: recipe.title,
+    publisher: recipe.publisher,
+    sourceUrl: recipe.source_url,
+    image: recipe.image_url,
+    servings: recipe.servings,
+    cookingTime: recipe.cooking_time,
+    ingredients: recipe.ingredients,
+    ...(recipe.key && {key: recipe.key}),
+  };
+}
+
 export const loadRecipe = async function (id) {
   try {
     const data = await getJSON(`${API_URL}${id}`);
+    state.recipe = createRecipeObject(data)
 
     //let {recipe} = data.data <--- the same as bottom
-    const recipe = data.data.recipe;
-    state.recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
+
     if (state.bookmarks.some(bookmark => bookmark.id === id))
       state.recipe.bookmarked = true;
     else state.recipe.bookmarked = false;
@@ -90,15 +96,49 @@ export const deleteBookmark = function (id) {
   persistBookmarks();
 };
 
-const init = function(){
-  const storage = localStorage.getItem('bookmarks')
-  if(storage) state.bookmarks = JSON.parse(storage)
-}
+const init = function () {
+  const storage = localStorage.getItem('bookmarks');
+  if (storage) state.bookmarks = JSON.parse(storage);
+};
 
-init()
+init();
 
-const clearBookmarks = function(){
-  localStorage.clear('bookmarks')
-}
+const clearBookmarks = function () {
+  localStorage.clear('bookmarks');
+};
 
 //clearBookmarks()
+
+export const uploadRecipe = async function (newRecipe) {
+  try {
+    const ingredients = Object.entries(newRecipe)
+      .filter(entry => entry[0].startsWith('ingredient') && entry[1] !== '')
+      .map(ing => {
+        const ingArr = ing[1].replaceAll(' ', '').split(',');
+        if (ingArr.length !== 3)
+          throw new Error(
+            'Wrong ingredient format! Please use the corect format!'
+          );
+        const [quantity, unit, description] = ingArr;
+
+        return { quantity: quantity ? +quantity : null, unit, description };
+      });
+
+
+    const recipe = {
+      title: newRecipe.title,
+      source_url: newRecipe.sourceUrl,
+      image_url: newRecipe.image,
+      publisher: newRecipe.publisher,
+      cooking_time: newRecipe.cookingTime,
+      servings: newRecipe.servings,
+      ingredients,
+    };
+  const data = await sendJSON(`${API_URL}?key=${KEY}`,recipe)
+  state.recipe = createRecipeObject(data);
+  addBookmark(state.recipe)
+  console.log(data)
+  } catch (err) {
+    throw err;
+  }
+};
